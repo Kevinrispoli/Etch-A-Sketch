2fd76433291948e45213ea6deec56e97c13e5cf7
Kevin Rispoli <kevin.rispoli@yahoo.com>
2024-10-29 13:21:58 -0400

    etch-a-Sketch completed
    completed etch-a-sketch using Html, Css, and Javascript

---
+++ java.js
@@ -0,0 +1,33 @@
+//javascript for etch-a-sketch
+
+let start = document.getElementById('start');
+let container = document.getElementById('container');
+
+createGrid(16);
+
+function createGrid(num) {
+    container.innerHTML = '';
+    for (i = 1; i <= num * num; i++) {
+        const box = document.createElement('div');
+        box.classList.add('boxes');
+        box.addEventListener('mouseenter', () => {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            let color = `rgb(${r}, ${g}, ${b})`;
+            box.style.backgroundColor = color;
+        })
+        container.appendChild(box);
+    }
+
+}
+
+start.addEventListener('click', () => {
+    let num = prompt("Enter the numbers of squares from 1 to 100")
+    if (num >= 1 && num <= 100) {
+        createGrid(num);
+    }
+    else {
+        alert("Enter number between 1 to 100");
+    }
+})
